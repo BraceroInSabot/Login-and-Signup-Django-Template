de42ce5dc62eae981467ed5070cdf407b880beea
Guilherme Bracero <guibragon@gmail.com>
2022-07-11 16:17:29 -0300

    added: index.html

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import SignUpView
 
+from core.views import index
+
 urlpatterns = [
     path("/auth/signup", SignUpView.as_view(template_name="signup.html"), name="sign"),
+    path("", index, name="home"),
 ]
--- core/views.py
+++ core/views.py
@@ -10,3 +10,7 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("sign")
+
+
+def index(request):
+    return render(request, "")
