4e65e56ede558c0197c6af96a26d320d3eb3c89a
Guilherme Bracero <guibragon@gmail.com>
2022-07-20 12:52:33 -0300

    docs | perf: docstrings added, user now have exclusivity on pages

--- core/views.py
+++ core/views.py
@@ -19,36 +19,49 @@ class PageView(TemplateView):
 
 
 def signup(request):
-    if request.method == "GET":
-        return render(request, "signup.html")
-    elif request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('c_password')
+    """
+    SignUp.
 
-        if password != confirm_password:
-            messages.add_message(request, constants.ERROR, "As suas senhas não batem!")
-            return redirect('signup')
+    This page is dedicated to user registration.
+    """
+    if not User.is_authenticated:
+        if request.method == "GET":
+            return render(request, "signup.html")
+        elif request.method == "POST":
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            confirm_password = request.POST.get('c_password')
 
-        if User.objects.filter(username=username).first():
-            # messages.error(request, "Esse nome de usuário já está sendo usado!")
-            messages.add_message(request, constants.ERROR, "Esse nome de usuário já está sendo usado!")
-            return redirect('signup')
-        elif User.objects.filter(email=email).first():
-            messages.add_message(request, constants.ERROR, "Esse endereço de e-mail já existe!")
-            return redirect('signup')
+            if password != confirm_password:
+                messages.add_message(request, constants.ERROR, "As suas senhas não batem!")
+                return redirect('signup')
 
-        new_user = User.objects.create_user(username=username, email=email, password=password, is_active=True,
-                                          is_superuser=False, is_staff=False)
+            if User.objects.filter(username=username).first():
+                # messages.error(request, "Esse nome de usuário já está sendo usado!")
+                messages.add_message(request, constants.ERROR, "Esse nome de usuário já está sendo usado!")
+                return redirect('signup')
+            elif User.objects.filter(email=email).first():
+                messages.add_message(request, constants.ERROR, "Esse endereço de e-mail já existe!")
+                return redirect('signup')
 
-        new_user.save()
+            new_user = User.objects.create_user(username=username, email=email, password=password, is_active=True,
+                                              is_superuser=False, is_staff=False)
 
-    messages.add_message(request, constants.SUCCESS, "PARABÉNS! Sua conta foi criada! Agora basta somente verificar o seu e-mail!")
-    return render(request, 'login.html')
+            new_user.save()
+
+        messages.add_message(request, constants.SUCCESS, "PARABÉNS! Sua conta foi criada! Agora basta somente verificar o seu e-mail!")
+        return render(request, 'login.html')
+    else:
+        return render(request, 'page.html')
 
 
 def login_auth(request):
+    """
+    Login.
+
+    This page is dedicated to user authentication.
+    """
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
